f3e7f91c2c4787f8510f4cf0e32d89405023674a
ge39yaz <ge39yaz@mytum.de>
2022-06-18 17:55:21 +0200

    Funktionen für die Spracherkennung hinzugefügt

---
+++ src/Spracherkennung.py
@@ -0,0 +1,55 @@
+import pyaudio
+import wave
+import numpy as np
+from stt import Model
+import time
+
+def create_audio(time):
+    chunk = 1024
+    format = pyaudio.paInt16
+    channels = 1
+    rate = 16000
+    record_seconds = time
+    file_name = "Spracheingabe.wav"
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format = format,
+                    channels = channels,
+                    rate = rate,
+                    input = True,
+                    frames_per_buffer = chunk)
+
+    print("Aufnahme gestartet")
+
+    frames = []
+
+    for i in range(0, int(rate / chunk * record_seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    print("Aufnahme beendet")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(file_name, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(format))
+    wf.setframerate(rate)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    return file_name
+
+def convert(model_path, recording_path):
+    lm = Model(model_path)
+    fin = wave.open(recording_path, 'rb')
+    audio = np.frombuffer(fin.readframes(fin.getnframes()), np.int16)
+    output = lm.stt(audio)
+    return output
+
+create_audio(5)
+time.sleep(2)
+print(convert('MODEL_PATH', 'RECORDING_PATH'))
