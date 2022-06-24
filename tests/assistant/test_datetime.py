93f9476b63408f45d775652330b37b21ad5d953e
Leopold Lüthe <leopold.luethe@tum.de>
2022-06-24 10:00:57 +0200

    FIX Coverage

--- tests/assistant/test_datetime.py
+++ tests/assistant/test_datetime.py
@@ -9,10 +9,17 @@ from assistant.timedate import timedate
 
 def test_time() -> None:
     """Test time function return current time."""
-    assert timedate('Wie viel Uhr ist es') == f'Es ist {dt.datetime.now().strftime("%H:%M")}'
+    assert timedate('Wie viel Uhr ist es') == \
+        f'Es ist {dt.datetime.now().strftime("%H:%M")}'
 
 
 def test_date() -> None:
     """Test date function return current date."""
     assert timedate('Welcher Tag ist heute') == \
         f'Es ist der {dt.datetime.now().strftime("%d/%m/%Y")}'
+
+
+def test_wrong_keyword() -> None:
+    """Test wrong Keyword handling."""
+    assert timedate('Wie heißt der Präsident der USA') == \
+        'Leider kann ich mit diesen Befehl nichts anfangen'
