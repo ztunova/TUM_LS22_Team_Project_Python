3251fbd5e84d66b5fea7d5e8ab30b2568abd4696
Zofia Tunova <zofka.tunova@gmail.com>
2022-06-24 23:30:53 +0200

    Final update

--- src/main.py
+++ src/main.py
@@ -5,19 +5,19 @@
 Source: https://stt.readthedocs.io/en/latest/Python-Examples.html
 """
 
-from __future__ import absolute_import, division, print_function
-
 import argparse
-import json
 import shlex
 import subprocess
 import sys
 import wave
-from timeit import default_timer as timer
-from typing import Tuple, Any
+from typing import TYPE_CHECKING
 
-import numpy as np
-from stt import Model
+if TYPE_CHECKING:
+    # pylint: disable=all
+    from typing import Any
+
+import numpy
+from stt import Model  # type: ignore
 
 try:
     from shlex import quote
@@ -25,22 +25,23 @@ except ImportError:
     from pipes import quote
 
 
-def convert_samplerate(audio_path: str, desired_sample_rate: type) -> Tuple[type, np.ndarray[Any, Any]]:
+def convert_samplerate(audio_path: str, ds_rate: str) -> tuple[str, numpy.ndarray[Any, Any]]:
     """Convert sample audio to desired sample rate."""
     sox_cmd = f'sox {quote(audio_path)} --type raw --bits 16 --channels 1\
-         --rate {desired_sample_rate} --encoding signed-integer --endian little\
+         --rate {ds_rate} --encoding signed-integer --endian little\
              --compression 0.0 --no-dither - '
     try:
         output = subprocess.check_output(shlex.split(sox_cmd), stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError as e:
-        raise RuntimeError(f'SoX returned non-zero status: {e.stderr}')
-    except OSError as e:
+    except subprocess.CalledProcessError as exception:
+        raise RuntimeError(f'SoX returned non-zero status: {exception.stderr}') from exception
+    except OSError as exception:
         raise OSError(
-            e.errno,
-            f'SoX not found, use {desired_sample_rate}hz files or install it: {e.strerror}',
-        )
+            exception.errno,
+            f'SoX not found, use {ds_rate}hz files or install it: {exception.strerror}',
+        ) from exception
 
-    return desired_sample_rate, np.frombuffer(output, np.int16)
+    return ds_rate, numpy.frombuffer(output, numpy.int16)
+
 
 def main() -> None:
     """Run to get text from audio file."""
@@ -83,61 +84,44 @@ def main() -> None:
     args = parser.parse_args()
 
     print(f'Loading model from file {args.model}', file=sys.stderr)
-    model_load_start = timer()
-    # sphinx-doc: python_ref_model_start
-    ds = Model(args.model)
-    # sphinx-doc: python_ref_model_stop
-    model_load_end = timer() - model_load_start
-    print(f'Loaded model in {model_load_end:.3}s.', file=sys.stderr)
+    data_model = Model(args.model)
 
     if args.beam_width:
-        ds.setBeamWidth(args.beam_width)
+        data_model.setBeamWidth(args.beam_width)
 
-    desired_sample_rate = ds.sampleRate()
+    desired_sample_rate = data_model.sampleRate()
 
     if args.scorer:
         print(f'Loading scorer from files {args.scorer}', file=sys.stderr)
-        scorer_load_start = timer()
-        ds.enableExternalScorer(args.scorer)
-        scorer_load_end = timer() - scorer_load_start
-        print(f'Loaded scorer in {scorer_load_end:.3}s.', file=sys.stderr)
+        data_model.enableExternalScorer(args.scorer)
 
         if args.lm_alpha and args.lm_beta:
-            ds.setScorerAlphaBeta(args.lm_alpha, args.lm_beta)
+            data_model.setScorerAlphaBeta(args.lm_alpha, args.lm_beta)
 
     if args.hot_words:
         print('Adding hot-words', file=sys.stderr)
         for word_boost in args.hot_words.split(','):
             word, boost = word_boost.split(':')
-            ds.addHotWord(word, float(boost))
+            data_model.addHotWord(word, float(boost))
 
     fin = wave.open(args.audio, 'rb')
     fs_orig = fin.getframerate()
     if fs_orig != desired_sample_rate:
         print(
-            ('Warning: original sample rate ({}) is different than {}hz. Resampling might produce\
-             erratic speech recognition.').format(
-                fs_orig, desired_sample_rate,
-            ),
+            (f'Warning: original sample rate ({fs_orig}) \
+                is different than {desired_sample_rate}hz. \
+                Resampling might produce \
+             erratic speech recognition.'),
             file=sys.stderr,
         )
         fs_new, audio = convert_samplerate(args.audio, desired_sample_rate)
     else:
-        audio = np.frombuffer(fin.readframes(fin.getnframes()), np.int16)
+        audio = numpy.frombuffer(fin.readframes(fin.getnframes()), numpy.int16)
 
-    audio_length = fin.getnframes() * (1 / fs_orig)
     fin.close()
 
     print('Running inference.', file=sys.stderr)
-
-    inference_start = timer()
-    print(ds.stt(audio))
-    inference_end = timer() - inference_start
-
-    print(
-        ('Inference took %0.3fs for %0.3fs audio file.') % (inference_end, audio_length),
-        file=sys.stderr,
-    )
+    print(data_model.stt(audio))
 
 
 if __name__ == '__main__':
