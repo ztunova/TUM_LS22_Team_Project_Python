8b3b429557ce27f5d214ea766d1928c40439e264
Leopold Lüthe <leopold.luethe@tum.de>
2022-06-25 13:40:00 +0200

    ADD Test and Volume Functions

--- src/assistant/volume.py
+++ src/assistant/volume.py
@@ -2,5 +2,69 @@
 # SPDX-License-Identifier: Apache-2.0
 """Volume Function."""
 
-import os 
-# import pyautogui
+import os
+
+import pyautogui
+
+STEP = 10
+OS = os.name
+
+
+def volume(taskinput: str) -> str:
+    """Use Keywords to find the right function to call.
+
+    Args:
+        taskinput: Input from keyword_find as a string
+
+    Returns:
+        Executed command or error.
+
+    """
+    up_list = ['lauter']
+    down_list = ['leiser']
+    mute_list = ['stumm', 'null']
+    keylist = [up_list, down_list, mute_list]
+
+    function_number = 0
+    for liste in keylist:
+        for element in liste:
+            if element in taskinput:
+                function_number = keylist.index(liste) + 1
+
+    if function_number == 1:
+        return louder()
+
+    if function_number == 2:
+        return quieter()
+
+    if function_number == 3:
+        return mute()
+
+    return 'Leider kann ich mit diesen Befehl nichts anfangen'
+
+
+def louder() -> str:
+    """Increase Volume."""
+    if OS == 'nt':
+        pyautogui.press('volumeup', presses=(STEP / 2))
+
+    if OS == 'posix':
+        os.system(f'pactl set-sink-volume @DEFAULT_SINK@ +{STEP}%')
+
+
+def quieter() -> str:
+    """Decrease Volume."""
+    if OS == 'nt':
+        pyautogui.press('volumedown', presses=(STEP / 2))
+
+    if OS == 'posix':
+        os.system(f'pactl set-sink-volume @DEFAULT_SINK@ -{STEP}%')
+
+
+def mute() -> str:
+    """Mute Volume."""
+    if OS == 'nt':
+        pyautogui.press('volumemute')
+
+    if OS == 'posix':
+        os.system('pactl set-sink-mute @DEFAULT_SINK@ toggle')
--- tests/assistant/test_volume.py
+++ tests/assistant/test_volume.py
@@ -2,6 +2,43 @@
 # SPDX-License-Identifier: Apache-2.0
 """Volume tests."""
 
-from unittest.mock import patch
+from os import name
 
-import assistant.volume
+from unittest.mock import patch
+from assistant.volume import volume
+
+
+def test_wrong_keyword() -> None:
+    """Test wrong Keyword handling."""
+    assert volume('Wie viel Uhr ist es') == \
+        'Leider kann ich mit diesen Befehl nichts anfangen'
+
+
+def test_win_up() -> None:
+    """Test Volume up for Windows."""
+    with patch()
+
+
+def test_win_down() -> None:
+    """Test Volume down for Windows."""
+    pass
+
+
+def test_win_mute() -> None:
+    """Test Volume mute for Windows."""
+    pass
+
+
+def test_lin_up() -> None:
+    """Test Volume up for Linux."""
+    pass
+
+
+def test_lin_down() -> None:
+    """Test Volume down for Linux."""
+    pass
+
+
+def test_lin_mute() -> None:
+    """Test Volume mute for Linux."""
+    pass
