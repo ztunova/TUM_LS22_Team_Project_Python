b8188719511af780279eecb167a19780641a3028
Thomas Schwarzfischer <thomas.schwarzfischer@tum.de>
2022-07-01 22:07:13 +0200

    Switch to class structure

--- tests/assistant/test_sprachausgabe.py
+++ tests/assistant/test_sprachausgabe.py
@@ -1,11 +1,12 @@
 """Tests for sprachausgabe.py."""
 
-from assistant.sprachausgabe import speak
+from assistant.sprachausgabe import TtsEngine
 
 
 def test_tts() -> None:
     """Play a test Strings. Does not actually test correctness."""
-    speak('Dies ist ein Test für die Sprachausgabe.')
+    engine = TtsEngine()
+    engine.speak('Dies ist ein Test für die Sprachausgabe.')
 
 
 if __name__ == '__main__':
